5e57606e6763cd39d03dc80ab57514ae114143f0
yu song <1963408085@qq.com>
2020-07-30 17:53:20 +0800

    修改为固定迭代次数并不/n,初始值为1

--- core/common/pagerank_c++2.h
+++ core/common/pagerank_c++2.h
@@ -16,7 +16,7 @@ vector <int> in_ver[MAX]; // 存储各点入度的点
 int out_num[MAX]; // 存储各点出度数量 
 float newPR[MAX]; // 存储pr 
 float oldPR[MAX];
-float d = 0.85;         // 初始化参数
+float d = 0.80;         // 初始化参数
 float threshold = 1e-4; // 迭代终止阈值
 
 void getPR(float *a, float *b, int n){
@@ -26,7 +26,8 @@ void getPR(float *a, float *b, int n){
 		for(int j = 0; j < len; j++){
 			a[i] += b[in_ver[i][j]] / out_num[in_ver[i][j]]; // 累加：该点入度点的pr值除以该点的出度 
 		}
-		a[i] = a[i] * d + (1 - d) / n; 
+		//a[i] = a[i] * d + (1 - d) / n;
+		a[i] = a[i] * d + 1 - d; 
 	}
 }
 
@@ -39,11 +40,12 @@ float* computePR(int n, char* outpath){
 				in_ver[j].push_back(i);
 			}
 		}
-		newPR[i] = 1.0/n;
+		//newPR[i] = 1.0/n;
+		newPR[i] = 1.0; // 初始化1
 	}  
     // 记录迭代次数
 	int cnt = 0;
-	while(1){
+	while(cnt < 200){
 		cnt++;
 		if(cnt % 2 == 1){
 			getPR(oldPR, newPR, n); // newPR迭代存入oldPR 
@@ -57,9 +59,9 @@ float* computePR(int n, char* outpath){
 			wc += abs(oldPR[i] - newPR[i]);
 		} 
 //		cout << cnt << ": " << wc << " " << threshold << endl; 
-		if(wc < threshold){
-			break;
-		}
+		//if(wc < threshold){ // 收敛判断
+		//	break;
+		//}
 	} 
 	// 将结果写入 
 	if(*outpath != '\0'){
